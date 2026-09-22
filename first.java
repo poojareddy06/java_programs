4323a413d71786da854b242f149dc8b134765675
poojareddy06 <poojavarra17@gmail.com>
2026-09-22 12:35:24 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,16 @@
+package ifelse;
+
+public class first {
+    public static void main(String[] args) {
+        int a=19;
+        if(a==18){
+            System.out.println("dog is barking");
+        }
+        else if(a==19){
+            System.out.println("dog is barking louder");
+        }
+        else{
+            System.out.println("dog is not barking");
+        }
+    }
+}
---
+++ second.java
@@ -0,0 +1,16 @@
+package ifelse;
+
+public class second {
+    public static void main(String[] args) {
+        int a=20;
+        if(a<=18){
+            System.out.println("dog is barking");
+        }
+        else if(a==19){
+            System.out.println("dog is barking louder");
+        }
+        else{
+            System.out.println("dog is not barking");
+        }
+    }
+}
